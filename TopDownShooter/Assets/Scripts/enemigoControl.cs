119d64cc92afb14b44a529bbf0600ac2ab08caf5
Macarena Ruiz <contactomruiz@yahoo.com>
2022-06-12 16:24:21 -0300

    enemigo follow

--- TopDownShooter/Assets/Scripts/enemigoControl.cs
+++ TopDownShooter/Assets/Scripts/enemigoControl.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemigoControl : MonoBehaviour
 {
+    public Transform player;
     private int Health = 2;
+    public float speed;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime); 
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Bala"))
